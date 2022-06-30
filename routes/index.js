bb7677323191292df8994fc32caf644b7b790edc
Jéssica Florentino <jdgflorentino@gmail.com>
2022-06-30 14:51:28 -0300

    feat:arquivo index de routes

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const productsRoutes = require('./productsRoutes');
+
+const routes = express.Router();
+
+routes.use('/products', productsRoutes);
+
+module.exports = routes;

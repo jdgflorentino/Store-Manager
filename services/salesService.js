ec8f46f239ab1968a045333b78bff44f4508a764
Jéssica Florentino <jdgflorentino@gmail.com>
2022-07-06 17:11:42 -0300

    feat:Camada Service rota GET /sales
    Criei função getAll para listar todas as vendas

--- services/salesService.js
+++ services/salesService.js
@@ -1,9 +1,14 @@
 const model = require('../models/Sales');
 
+const getAll = async () => { 
+  const sales = await model.getAll();
+  return sales;
+};
+
 const create = async (newSales) => { 
   const insertId = await model.create();
   await Promise.all(newSales.map(async (sale) => model.newSale(insertId, sale)));
   return insertId;
 };
 
-module.exports = { create };
+module.exports = { create, getAll };

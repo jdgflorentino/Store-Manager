5017cbaec3379b0153f05105fe96c2144e593a01
Jéssica Florentino <jdgflorentino@gmail.com>
2022-07-06 17:15:52 -0300

    feat:Camada Controller rota GET /sales e GET /sales/:id
    Criei as funções getAll e getById para buscar as vendas

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,9 +1,19 @@
 const service = require('../services/salesService');
 
+const getById = async (req, res) => { 
+  const sale = await service.getById(req.params.id);
+  return res.status(200).json(sale);
+};
+
+const getAll = async (req, res) => { 
+  const sales = await service.getAll();
+  return res.status(200).json(sales);
+};
+
 const create = async (req, res) => {
   const result = await service.create(req.body);
   console.log(result);
   return res.status(201).json({ id: result, itemsSold: req.body });
 };
 
-module.exports = { create };
+module.exports = { create, getAll, getById };
